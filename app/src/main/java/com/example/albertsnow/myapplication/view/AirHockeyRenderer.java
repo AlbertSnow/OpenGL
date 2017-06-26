cfd2b9f5eaa2da7f22871992fd9e57eda375091f
AlbertSnow <Albert.Snow.Email@gmail.com>
2017-06-26 16:55:55 +0800

    1. padding gradual color to the table of the game (use varying type) 2.use single array to contain multi data type (position color)

--- app/src/main/java/com/example/albertsnow/myapplication/view/AirHockeyRenderer.java
+++ app/src/main/java/com/example/albertsnow/myapplication/view/AirHockeyRenderer.java
@@ -20,13 +20,17 @@ import javax.microedition.khronos.opengles.GL10;
  */
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
-    private static final String U_COLOR = "u_Color";
     private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4;
+    private static final int STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
-    private int uColorLocation;
+    private int aColorLocation;
 
     private static final String A_POSITION = "a_Position";
+    private static final String A_COLOR = "a_Color";
+
     private int aPositionLocation;
 
     private int program;
@@ -34,30 +38,25 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
 
     public AirHockeyRenderer() {
-        float[] tableVertices = {
-                0f, 0f,
-                0f, 14f,
-                9f, 14f,
-                9f, 0f
-        };
         float[] tableVerticesWithTriangles = {
-                // Triangle 1
-                -0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
+                // Order of coordinates: X, Y, R, G, B
 
-                //Triangle 2
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
+                // Triangle Fan
+                0.0f, 0.0f, 1f, 1f, 1f,
+
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
 
                 //Line 1
-                -0.5f, 0f,
-                0.5f, 0f,
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
 
                 // Mallets
-                0f, -0.25f,
-                0f, 0.25f
+                0f, -0.25f, 0f, 0f, 1f,
+                0f, 0.25f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -83,13 +82,18 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         ShaderHelper.validateProgram(program);
         GLES20.glUseProgram(program);
 
-        uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR);
         aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
+        aColorLocation = GLES20.glGetAttribLocation(program, A_COLOR);
 
         vertexData.position(0);
         GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
-            false, 0, vertexData);
+            false, STRIDE, vertexData);
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        GLES20.glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT,
+                false, STRIDE, vertexData);
+
         GLES20.glEnableVertexAttribArray(aPositionLocation);
+        GLES20.glEnableVertexAttribArray(aColorLocation);
     }
 
 
@@ -102,18 +106,13 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
-        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        //draw triangle, 0 offset, 6 vertices count
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
 
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
 
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
 
-        GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1);
 
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1);
     }
 
