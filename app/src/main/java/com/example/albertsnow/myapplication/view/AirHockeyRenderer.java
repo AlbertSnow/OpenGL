06cc0c24cdc9ee2ad03e2c8a6e09659e327905ce
AlbertSnow <Albert.Snow.Email@gmail.com>
2017-06-27 16:46:51 +0800

    1. use matrix transform position to normal device coordinate

--- app/src/main/java/com/example/albertsnow/myapplication/view/AirHockeyRenderer.java
+++ app/src/main/java/com/example/albertsnow/myapplication/view/AirHockeyRenderer.java
@@ -2,6 +2,7 @@ package com.example.albertsnow.myapplication.view;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import com.example.albertsnow.myapplication.MyApplication;
 import com.example.albertsnow.myapplication.R;
@@ -35,6 +36,10 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     private int program;
     private final FloatBuffer vertexData;
+    private static final String U_MATRIX = "u_Matrix";
+    private final float[] projectionMatrix = new float[16];
+    private int uMatrixLocation;
+
 
 
     public AirHockeyRenderer() {
@@ -44,19 +49,19 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
                 // Triangle Fan
                 0.0f, 0.0f, 1f, 1f, 1f,
 
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
 
                 //Line 1
                 -0.5f, 0f, 1f, 0f, 0f,
                 0.5f, 0f, 1f, 0f, 0f,
 
                 // Mallets
-                0f, -0.25f, 0f, 0f, 1f,
-                0f, 0.25f, 1f, 0f, 0f
+                0f, -0.4f, 0f, 0f, 1f,
+                0f, 0.4f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -84,6 +89,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
         aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
         aColorLocation = GLES20.glGetAttribLocation(program, A_COLOR);
+        uMatrixLocation = GLES20.glGetUniformLocation(program, U_MATRIX);
 
         vertexData.position(0);
         GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
@@ -100,11 +106,21 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         gl.glViewport(0, 0, width, height);
+        final float aspectRatio = width > height ?
+                (float) width / (float)height :
+                (float) height / (float) width;
+        if (width > height) {
+            Matrix.orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            Matrix.orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
 
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
